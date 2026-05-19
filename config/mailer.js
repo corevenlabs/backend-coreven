51dc14af3a33b31ee961d727a2932701549d832d
corevenlabs <corevenlabs@gmail.com>
2026-05-19 14:50:02 -0400

    cambios

--- config/mailer.js
+++ config/mailer.js
@@ -2,16 +2,17 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "74.125.133.108",
     port: 587,
     secure: false,
-    requireTLS: true,
+
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_PASS
     },
+
     tls: {
-        family: 4
+        rejectUnauthorized: false
     }
 });
 
